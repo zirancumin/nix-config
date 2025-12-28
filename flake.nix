7500f2656140501bce60108ce4d0c8add964e33f
Zi Ran <sdfox05@gmail.com>
2025-12-28 21:56:58 +0800

    drop home-manager

--- flake.nix
+++ flake.nix
@@ -3,11 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     # Used with `nixos-rebuild switch --flake .#<hostname>`
     # nixosConfigurations."<hostname>".config.system.build.toplevel must be a derivation
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
@@ -24,18 +22,6 @@
         ./modules/coq.nix
         ./modules/unfree.nix
         ./modules/riscv.nix
-
-        # Home Manager
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.sdfox = ./home/sdfox.nix;
-          home-manager.users.ziran = ./home/ziran.nix;
-
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
       ];
     };
   };
